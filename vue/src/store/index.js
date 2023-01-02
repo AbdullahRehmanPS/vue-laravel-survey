68d982f0c00aef963bcb540917e32eb36fd5802c
Mac-25 <abdullahrehman.se@gmail.com>
2023-01-02 15:09:43 +0500

    Setup vuex, tailwindcss & vue router

---
+++ vue/src/store/index.js
@@ -0,0 +1,14 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+  state: {
+    user: {
+      data: {},
+      token: null
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {}
+})
+export default store;
