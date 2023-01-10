738b83cf21486a999022c04951310b9595157608
Mac-25 <abdullahrehman.se@gmail.com>
2023-01-10 18:20:56 +0500

    Worked on signuo and login

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -14,12 +14,12 @@
           </router-link>
         </p>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <form class="mt-8 space-y-6" @submit="register">
         <input type="hidden" name="remember" value="true" />
         <div class="-space-y-px rounded-md shadow-sm">
           <div>
             <label for="full-name" class="sr-only">Full name</label>
-            <input id="full-name" name="name" type="text" autocomplete="name" required
+            <input id="full-name" name="name" type="text" autocomplete="name" required="" v-model="user.name"
                    class="relative block w-full
                    appearance-none rounded-none
                    rounded-t-md border border-gray-300
@@ -31,7 +31,7 @@
           </div>
           <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required=""
+            <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email"
                    class="relative block
                    w-full appearance-none
                    rounded-none border border-gray-300
@@ -44,7 +44,20 @@
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required=""
+            <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="user.password"
+                   class="relative block w-full
+                   appearance-none rounded-none
+                   border border-gray-300
+                   px-3 py-2 text-gray-900 placeholder-gray-500
+                   focus:z-10 focus:border-indigo-500
+                   focus:outline-none focus:ring-indigo-500
+                   sm:text-sm"
+                   placeholder="Password" />
+          </div>
+          <div>
+            <label for="password_confirmation" class="sr-only">Confirm Password</label>
+            <input id="password_confirmation" name="password_confirmation" type="password"
+                   autocomplete="current-password_confirmation" required="" v-model="user.password_confirmation"
                    class="relative block w-full
                    appearance-none rounded-none
                    rounded-b-md border border-gray-300
@@ -52,7 +65,7 @@
                    focus:z-10 focus:border-indigo-500
                    focus:outline-none focus:ring-indigo-500
                    sm:text-sm"
-                   placeholder="Password" />
+                   placeholder="Confirm Password" />
           </div>
         </div>
 
@@ -78,4 +91,26 @@
 
 <script setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
+import {useRouter} from "vue-router";
+import store from "../store/index.js";
+
+const router = useRouter();
+const user = {
+  name: '',
+  email: '',
+  password: '',
+  password_confirmation: ''
+};
+
+function register(ev) {
+  ev.preventDefault();
+  store
+    .dispatch('register', user)
+    .then(() => {
+      router.push({
+        name: 'Dashboard'
+      })
+    })
+}
+
 </script>
